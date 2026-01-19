3aa53300b9572b876cebff20f1c9c735edf9aceb
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2026-01-19 14:08:09 -0300

    ajustes para entrega

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,13 +1,12 @@
-import next from 'next';
 import { NextRequest, NextResponse } from 'next/server'
 
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const {pathname} = request.url;
+  const {pathname} = request.nextUrl;
 
-  if((pathname=== '/dashboard' || pathname === '/carrito') && !request.cookies.get('userSession')?.value) {
-
+  if((pathname === '/dashboard' || pathname === '/carrito') && !request.cookies.get('userSession')?.value) {
+    return NextResponse.redirect(new URL('/', request.url))
   } else {
     return NextResponse.next();
   }
